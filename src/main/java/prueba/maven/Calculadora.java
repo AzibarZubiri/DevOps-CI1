2a3ab0727352a8a09469294d7cd841b255b24312
Azibar <agonzalezme@zubirimanteo.com>
2025-09-28 19:52:03 +0200

    Proyecto Maven creado con test para el CI

---
+++ src/main/java/prueba/maven/Calculadora.java
@@ -0,0 +1,13 @@
+package prueba.maven;
+
+public class Calculadora {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }   
+    
+}
